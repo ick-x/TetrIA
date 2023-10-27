d98eac0574e8d95564ede5c5af40d86321a32357
roms <romaingroultlgc@gmail.com>
2023-10-27 13:08:55 +0200

    FIX Game

--- mainTest.py
+++ mainTest.py
@@ -1,7 +1,28 @@
 import tetris2 as tt
+import pygame as pg
 
 grid = tt.Grid()
-
 grid.move_down()
-grid.save_piece()
-print(grid.grid_color)
+grid.move_down()
+
+pg.init()
+screen = pg.display.set_mode((960, 540))
+
+running = True
+
+ct = 0
+
+while running:
+    if ct ==1000:
+        ct = 0
+        grid.move_down()
+    ct+=1
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    grid.paint(screen, 30)
+
+    pg.display.flip()
+
+pg.quit()
--- tetris2.py
+++ tetris2.py
@@ -2,24 +2,19 @@ import numpy as np
 import pygame as pg
 
 class Piece :
-    grid = np.array([[True, True, True],[False,False,True]])
+    grid = np.array([[True, False, True],[False,True,False]])
     x = 0
-    y = 0
-    color = pg.Color(0,0,0)
+    y = -2
+    color = pg.Color(255,0,0)
     
     def paint(self, tile_size, marge, screen):
         marge_x, marge_y = marge
-        
-        coord_piece_x, coord_piece_y = (marge_x + self.x*tile_size, marge_y+(self.y-1)*tile_size)
-        
-        tile_height = len(self.grid)
-        
-        for y in range(tile_height):
+
+        for y in range(len(self.grid)):
             for x in range(len(self.grid[0])):
                 if(self.grid[y][x]):
-                    coord_tile_x = coord_piece_x + x * tile_size
-                    coord_tile_y = coord_piece_y + (y - tile_height) * tile_size
-                
+                    coord_tile_x = (self.x + x) * tile_size + marge_x
+                    coord_tile_y = (self.y + y) * tile_size + marge_y
                     pg.draw.rect(screen, self.color, pg.Rect(coord_tile_x,coord_tile_y, tile_size, tile_size))
               
                 
@@ -29,8 +24,8 @@ class Piece :
         
         
 class Grid:
-    x = 12/2
-    y = 22/2
+    x = 3
+    y = 4
     gridBorderWidth = 4
     
     current_piece = Piece()
@@ -41,16 +36,29 @@ class Grid:
 
     def __init__(self):
         self.grid_color = [[pg.Color(155, 155, 155) for i in range(int(self.x))] for j in range(int(self.y))]
+        self.grid_color[3][1] = pg.Color(0,0,0)
         pass
 
     def save_piece(self):
         piece = self.current_piece
-        for x in range(len([piece.grid])):
-            for y in range(len(piece.grid[0])):
-                if piece.grid[x][y]:
-                    self.grid_color[int(piece.y - len(piece.grid) + y)][int(x + piece.x)] = piece.color
-                y += 1
-            x += 1
+        for y in range(len(piece.grid)):
+            for x in range(len(piece.grid[0])):
+                if piece.grid[y][x]:
+                    new_x = int(piece.x+x)
+                    new_y = int(piece.y+y)
+                    if(new_x>=0 and new_y>=0):
+                        self.grid_color[new_y][new_x]= piece.color
+
+    def print(self):
+        grid = ""
+        for list in self.grid_color:
+            for color in list:
+                if color == pg.Color(155,155,155):
+                    grid+="-"
+                else:
+                    grid+="o"
+            grid+="\n"
+        print(grid)
 
     def generate_piece(self):
         self.pieces.pop()
@@ -58,43 +66,32 @@ class Grid:
 
     def check_collision(self):
         piece = self.current_piece
-        if piece.y >= self.y:
-            self.save_piece()
-            self.generate_piece()
+
+        if piece.y + len(piece.grid) >= self.y :
             return True
 
-        x = 0
-        for line in piece.grid:
-            y = -len(piece.grid)
-            for bool in line:
-                if bool:
-                    if self.grid_bool[y + int(piece.y)][x + int(piece.x)]:
-                        self.save_piece()
-                        self.generate_piece()
-                        return True
-                y += 1
-            x += 1
+        for y in range(len(piece.grid)):
+            for x in range(len(piece.grid[0])):
+                if piece.grid[y][x] == True:
+                    new_y = int(piece.y+y)
+                    new_x = int(piece.x+x)
+                    if new_x >= 0 and new_y >= 0:
+                        if self.grid_color[new_y][new_x] != pg.Color(155,155,155):
+                            return True
 
     def paint(self, screen, tile_size):
-        pos_x = (screen.get_width() - tile_size * self.x) / 2
-        pos_y = (screen.get_height() - tile_size * self.y) / 2
+        pos_x = (screen.get_width() - tile_size * int(self.x)) / 2
+        pos_y = (screen.get_height() - tile_size * int( self.y)) / 2
 
-        pg.draw.rect(screen, pg.Color(0, 0, 0),
+        pg.draw.rect(screen, pg.Color(255, 255, 255),
                          pg.Rect(pos_x - self.gridBorderWidth, pos_y - self.gridBorderWidth,
                                      self.x * tile_size + self.gridBorderWidth * 2,
                                      self.y * tile_size + self.gridBorderWidth * 2))
-        pg.draw.rect(screen, pg.Color(255, 255, 255),
+        pg.draw.rect(screen, pg.Color(155, 155, 155),
                          pg.Rect(pos_x, pos_y, self.x * tile_size, self.y * tile_size))
 
-        self.pieces[-1].paint(tile_size, (pos_x, pos_y), screen)
-        self.check_collision()
-
-        for y in range(len(self.grid_bool)):
-            for x in range(len(self.grid_bool[0])):
-                if (self.grid_bool[y][x]):
-                    pg.draw.rect(screen, self.grid_color[y][x], pg.Rect(
-                        pos_x + (x) * tile_size, (y-1) * tile_size + pos_y, tile_size, tile_size
-                    ))
+        self.current_piece.paint(tile_size, (pos_x, pos_y), screen)
+        
 
     def move_left(self):
         piece = self.current_piece
@@ -108,17 +105,18 @@ class Grid:
 
     def move_down(self):
         piece = self.current_piece
-        if piece.y > 0:
-            piece.y += 0.25
+        if not(self.check_collision()):
+            piece.y += 1
             
     def move_instant(self):
         piece = self.current_piece
-        while not self.check_collision(self.current_piece):
-            piece.y += 0.1          
+        while not self.check_collision():
+            piece.y += 0.1
+        piece.y -= 0.1
         
     def rotate(self):
         grid = self.current_piece
-        array = np.array([grid[i]for i in range(len(grid))])
+        array = np.array([grid[i] for i in range(len(grid))])
         self.current_piece.grid = array.T
         
     def update(self, screen, tile_size):
